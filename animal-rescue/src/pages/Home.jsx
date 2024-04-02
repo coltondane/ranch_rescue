51867c6dbe82ebfdb51b9521ba951d764d48315f
coltondane <113216904+coltondane@users.noreply.github.com>
2024-04-02 00:43:18 -0600

    adding some sample content

--- animal-rescue/src/pages/Home.jsx
+++ animal-rescue/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 // HomePage.js
 import React from 'react';
+import '../style/home.css';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to Our Animal Rescue</h1>
-      <p>Find your new furry friend here!</p>
+    <div className='content'>
+      <h1>Rescue, Rehabilitate, Rehome</h1>
+      <p>Lily's Ranch and Rescue is a 501(c)(3) Animal Sanctuary that was formerly based in Colorado. We are now located in the beautiful blue grass valleys of Kentucky. Our new location provides our current animal residents  with more land to roam freely and allows us to intake more animals in need. Lily's Ranch and Rescue focuses on the rescue, rehabilitation and re-homing of all types of farm animals. Here at Lily’s Ranch & Rescue, we provide specialized veterinary care, farrier services, custom feed, and most importantly, the tender love for all of our animals.</p>
     </div>
   );
 }
