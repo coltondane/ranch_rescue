81d374c27bfa74dacdb3cb5921f3dfd6cdb0ee86
coltondane <113216904+coltondane@users.noreply.github.com>
2024-04-09 17:34:11 -0600

    adding base content from previous website

--- animal-rescue/src/pages/Home.jsx
+++ animal-rescue/src/pages/Home.jsx
@@ -14,10 +14,16 @@ const HomePage = () => {
                         We are a non-profit organization that rescues and rehabilitates horses and other animals. We are dedicated to providing a safe and loving environment for animals in need. Our mission is to rescue, rehabilitate, and rehome animals in need. We believe that all animals deserve a second chance at life and we are committed to helping them find their forever homes.
                     </p>
                 </Col>
+            </Row>
+            <Row>
                 <Col>
+                    <h2>Why We Do What We Do</h2>
                     <p>
-                        We are a non-profit organization that rescues and rehabilitates horses and other animals. We are dedicated to providing a safe and loving environment for animals in need. Our mission is to rescue, rehabilitate, and rehome animals in need. We believe that all animals deserve a second chance at life and we are committed to helping them find their forever homes.
-                    </p>  
+                    To make a long story short, the animals are our reason. We have made the choice to be the voice for the voiceless. These animals who are neglected, abused, and pushed aside who are rescued by us remind us just how important our role is to be their voice. To be the ones to speak up on their behalf and educate others on the traumatic abuse that occurs to a number of animals daily. Not only is it important to us to be their voices, but it is equally as important to us that we show the animals the love they never received. At Lily's Ranch and Rescue, we pride ourselves on the healing love and care that we provide each animal that joins us.
+                    </p>
+                </Col>
+                <Col>
+                    <img src={horsesImg} alt='horses' className='img-fluid' />
                 </Col>
             </Row>
         </Container>
