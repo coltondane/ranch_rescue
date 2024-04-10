a090e9e8d90aeed6197b086f8091438bdcc7e275
coltondane <113216904+coltondane@users.noreply.github.com>
2024-04-09 19:08:43 -0600

    working more styling

--- animal-rescue/src/pages/AboutUs.jsx
+++ animal-rescue/src/pages/AboutUs.jsx
@@ -1,13 +1,29 @@
 // AboutPage.js
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-// import '../style/about.css';
+import '../style/aboutUs.css';
 import horsesImg from '../assets/horses.jpeg';
+import lilyImg from '../assets/lily.jpg';
 
 
 const AboutPage = () => {
   return (
     <Container className='content'>
+      <Row className='title-lily'>
+        <Col>
+          <h1 className='meet'>Meet Lily, Our First</h1>
+        </Col>
+      </Row>
+      <Row className='summary-lily'>
+        <Col>
+          <p>
+            Every great story starts somewhere, and ours starts with a miniature pony-donkey cross named Lily. Rescued from a feedlot as a filly, she was the first of many rescues to grace our farm. Lily is full of spice and after spending her first few years with us at our rescue, she has lovingly found her forever home with a family in Colorado. It was love at first sight for Laurie and Lily. It was Lily who inspired Laurie to not only take in animals in need, but also find homes for animals who have a chance at finding families who would love them with all of their hearts. Rehoming animals allows us to intake new rescues, and those who cannot find a home due to health conditions or other complications will always have a safe, forever home here at Lily's Ranch and Rescue.
+          </p>
+        </Col>
+        <Col>
+          <img src={lilyImg} alt='horses' className='lily' />
+        </Col>
+      </Row>
       <Row className='reason'>
         <Col>
           <h2>Why We Do What We Do</h2>
