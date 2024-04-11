ecb4d049049450acb47611a09f6f514c36626645
coltondane <113216904+coltondane@users.noreply.github.com>
2024-04-11 14:00:36 -0600

    adding a utilities folder for helpers, just implemented email validation that will be used for the contact page

---
+++ animal-rescue/src/utils/helpers.js
@@ -0,0 +1,7 @@
+// Helper functions
+
+// validate email
+export function validateEmail(email) {
+    var re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(String(email).toLowerCase());
+  }
