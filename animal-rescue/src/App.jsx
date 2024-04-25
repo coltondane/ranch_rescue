63cabf2f916c4ebe2eb94be554f77666953c79c6
coltondane <113216904+coltondane@users.noreply.github.com>
2024-04-25 14:46:42 -0600

    applied original styling to buttons along with applying click action to entire area

--- animal-rescue/src/App.jsx
+++ animal-rescue/src/App.jsx
@@ -1,49 +1,49 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React from "react";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./style/app.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ranchLogo from './assets/ranchLogo.jpg';
-import Popup from './components/Popup.jsx';
-import Home from './pages/Home';
-import AboutUs from './pages/AboutUs';
-import Gallery from './pages/Gallery.jsx';
-import ContactUs from './pages/ContactUs';
-import Donate from './pages/Donate';
+import "bootstrap/dist/css/bootstrap.min.css";
+import ranchLogo from "./assets/ranchLogo.jpg";
+import Popup from "./components/Popup.jsx";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Gallery from "./pages/Gallery.jsx";
+import ContactUs from "./pages/ContactUs";
+import Donate from "./pages/Donate";
 
 function App() {
   // Set default page state to 'Home'
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState("Home");
 
   // Set the initial page to 'Home' when the component mounts
   useEffect(() => {
-    setCurrentPage('Home');
+    setCurrentPage("Home");
   }, []);
   return (
     <Router>
       <div className="rescue-app">
         <header className="header-container">
-          <h1 className='name'>
-            <img src={ranchLogo} alt='ranch logo' className='logo' />
+          <h1 className="name">
+            <img src={ranchLogo} alt="ranch logo" className="logo" />
             Lily's Ranch and Rescue
           </h1>
           <nav className="navigation">
             <ul>
-              <button>
-                <Link to="/" className="home-btn">Home</Link>
-              </button>
-              <button>
-                <Link to="/about" className="about-btn">About Us</Link>
-              </button>
-              <button>
-                <Link to="/gallery" className="gallery-btn">Gallery</Link>
-              </button>
-              <button>
-                <Link to="/contact" className="contact-btn">Contact Us</Link>
-              </button>
-              <button>
-                <Link to="/donate" className='donate-btn'>Donate</Link>
-              </button>
+              <Link to="/" className="link-style">
+                <button className="home-btn">Home</button>
+              </Link>
+              <Link to="/about" className="link-style">
+                <button className="about-btn">About Us</button>
+              </Link>
+              <Link to="/gallery" className="link-style">
+                <button className="gallery-btn">Gallery</button>
+              </Link>
+              <Link to="/contact" className="link-style">
+                <button className="contact-btn">Contact Us</button>
+              </Link>
+              <Link to="/donate" className="link-style">
+                <button className="donate-btn">Donate</button>
+              </Link>
             </ul>
           </nav>
         </header>
@@ -58,16 +58,28 @@ function App() {
         </div>
         <footer>
           <div>
-            <a href="https://www.facebook.com/lilysranchandrescue/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.facebook.com/lilysranchandrescue/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fab fa-facebook"></i>
             </a>
             <a href="__" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-patreon"></i>
             </a>
-            <a href="https://www.tiktok.com/@lilysranchandrescue" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.tiktok.com/@lilysranchandrescue"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fab fa-tiktok"></i>
             </a>
-            <a href="https://www.instagram.com/lilysranchandrescue/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.instagram.com/lilysranchandrescue/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fab fa-instagram"></i>
             </a>
           </div>
