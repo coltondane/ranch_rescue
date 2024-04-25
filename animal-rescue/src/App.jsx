3f73911774862d00278f82fb1d3099e828f3db1e
coltondane <113216904+coltondane@users.noreply.github.com>
2024-04-25 12:23:53 -0600

    refactoring site routing to use react router instead of state changes to implement internal linking through more areas opposed to just the nav bar

--- animal-rescue/src/App.jsx
+++ animal-rescue/src/App.jsx
@@ -1,95 +1,170 @@
-// root imports
-import {useState} from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import "./style/app.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import popup component
+import ranchLogo from './assets/ranchLogo.jpg';
 import Popup from './components/Popup.jsx';
-
-// import pages and other 
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import Gallery from './pages/Gallery.jsx';
 import ContactUs from './pages/ContactUs';
 import Donate from './pages/Donate';
 
-import ranchLogo from './assets/ranchLogo.jpg';
-
-// function to set the current page
 function App() {
-  // set default state along with state setter
-  const [currentPage, setCurrentPage] = useState('Home');
-
-  const renderHome = () => {
-      setCurrentPage('Home');
-  };
-  const renderAboutUs = () => {
-      setCurrentPage('About Us');
-  }
-  const renderDonate = () => {
-      setCurrentPage('Donate');
-  };
-  const renderContactUs = () => {
-      setCurrentPage('Contact Us');
-  };
-  const renderAnimals = () => {
-      setCurrentPage('Animals');
-  };
-
   return (
-    <div className="rescue-app">
-      <header className="header-container">
-        <h1 className='name'>
-          <img src={ranchLogo} alt='ranch logo' className='logo' />
-          Lily's Ranch and Rescue
-        </h1>
-        <nav className="navigation">
-          <ul>
-            <button onClick={renderHome} className="home-btn">
-              Home
-            </button>
-            <button onClick={renderAboutUs} className="about-btn">
-              About Us
-            </button>
-            <button onClick={renderAnimals} className="animals-btn">
-              Gallery
-            </button>
-            <button onClick={renderContactUs} className="contact-btn">
-              Contact Us
-            </button>
-            <button onClick={renderDonate} className="donate-btn">
-              Donate 
-            </button>
-          </ul>
-        </nav>
-      </header>
-      <div className="content">
-        {currentPage === "Home" && <Home />}
-        {currentPage === "About Us" && <AboutUs />}
-        {currentPage === "Animals" && <Gallery />}
-        {currentPage === "Contact Us" && <ContactUs />}
-        {currentPage === "Donate" && <Donate />}
-      </div>
-      <footer>
-        <div>
-        <a href="https://www.facebook.com/lilysranchandrescue/" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-facebook"></i>
-        </a>
-        <a href="__" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-patreon"></i>
-        </a>
-        <a href="https://www.tiktok.com/@lilysranchandrescue" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-tiktok"></i>
-        </a>
-        <a href="https://www.instagram.com/lilysranchandrescue/" target="_blank" rel="noopener noreferrer">
-          <i className="fab fa-instagram"></i>
-        </a>
+    <Router>
+      <div className="rescue-app">
+        <header className="header-container">
+          <h1 className='name'>
+            <img src={ranchLogo} alt='ranch logo' className='logo' />
+            Lily's Ranch and Rescue
+          </h1>
+          <nav className="navigation">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/gallery">Gallery</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li>
+              <li>
+                <Link to="/donate">Donate</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <div className="content">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={AboutUs} />
+            <Route path="/gallery" component={Gallery} />
+            <Route path="/contact" component={ContactUs} />
+            <Route path="/donate" component={Donate} />
+          </Switch>
         </div>
-        <p>&copy; 2024 Lily's Ranch and Rescue 501(c)(3)</p>
-      </footer>
-      <Popup />
-    </div>
+        <footer>
+          <div>
+            <a href="https://www.facebook.com/lilysranchandrescue/" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a href="__" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-patreon"></i>
+            </a>
+            <a href="https://www.tiktok.com/@lilysranchandrescue" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-tiktok"></i>
+            </a>
+            <a href="https://www.instagram.com/lilysranchandrescue/" target="_blank" rel="noopener noreferrer">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </div>
+          <p>&copy; 2024 Lily's Ranch and Rescue 501(c)(3)</p>
+        </footer>
+        <Popup />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+// // root imports
+// import {useState} from 'react';
+// import "./style/app.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// // import popup component
+// import Popup from './components/Popup.jsx';
+
+// // import pages and other 
+// import Home from './pages/Home';
+// import AboutUs from './pages/AboutUs';
+// import Gallery from './pages/Gallery.jsx';
+// import ContactUs from './pages/ContactUs';
+// import Donate from './pages/Donate';
+
+// import ranchLogo from './assets/ranchLogo.jpg';
+
+// // function to set the current page
+// function App() {
+//   // set default state along with state setter
+//   const [currentPage, setCurrentPage] = useState('Home');
+
+//   const renderHome = () => {
+//       setCurrentPage('Home');
+//   };
+//   const renderAboutUs = () => {
+//       setCurrentPage('About Us');
+//   }
+//   const renderDonate = () => {
+//       setCurrentPage('Donate');
+//   };
+//   const renderContactUs = () => {
+//       setCurrentPage('Contact Us');
+//   };
+//   const renderAnimals = () => {
+//       setCurrentPage('Animals');
+//   };
+
+//   return (
+//     <div className="rescue-app">
+//       <header className="header-container">
+//         <h1 className='name'>
+//           <img src={ranchLogo} alt='ranch logo' className='logo' />
+//           Lily's Ranch and Rescue
+//         </h1>
+//         <nav className="navigation">
+//           <ul>
+//             <button onClick={renderHome} className="home-btn">
+//               Home
+//             </button>
+//             <button onClick={renderAboutUs} className="about-btn">
+//               About Us
+//             </button>
+//             <button onClick={renderAnimals} className="animals-btn">
+//               Gallery
+//             </button>
+//             <button onClick={renderContactUs} className="contact-btn">
+//               Contact Us
+//             </button>
+//             <button onClick={renderDonate} className="donate-btn">
+//               Donate 
+//             </button>
+//           </ul>
+//         </nav>
+//       </header>
+//       <div className="content">
+//         {currentPage === "Home" && <Home />}
+//         {currentPage === "About Us" && <AboutUs />}
+//         {currentPage === "Animals" && <Gallery />}
+//         {currentPage === "Contact Us" && <ContactUs />}
+//         {currentPage === "Donate" && <Donate />}
+//       </div>
+//       <footer>
+//         <div>
+//         <a href="https://www.facebook.com/lilysranchandrescue/" target="_blank" rel="noopener noreferrer">
+//           <i className="fab fa-facebook"></i>
+//         </a>
+//         <a href="__" target="_blank" rel="noopener noreferrer">
+//           <i className="fab fa-patreon"></i>
+//         </a>
+//         <a href="https://www.tiktok.com/@lilysranchandrescue" target="_blank" rel="noopener noreferrer">
+//           <i className="fab fa-tiktok"></i>
+//         </a>
+//         <a href="https://www.instagram.com/lilysranchandrescue/" target="_blank" rel="noopener noreferrer">
+//           <i className="fab fa-instagram"></i>
+//         </a>
+//         </div>
+//         <p>&copy; 2024 Lily's Ranch and Rescue 501(c)(3)</p>
+//       </footer>
+//       <Popup />
+//     </div>
+//   );
+// }
+
+// export default App;
--- animal-rescue/src/pages/Home.jsx
+++ animal-rescue/src/pages/Home.jsx
@@ -2,8 +2,9 @@
 import React from 'react';
 import '../style/home.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-function HomePage() {
+function HomePage({renderHome, renderAboutUs, renderAnimals, renderContactUs, renderDonate}) {
     return (
         <Container className='content'>
             <Row className='intro'>
@@ -13,7 +14,7 @@ function HomePage() {
                         Lily's Ranch and Rescue is a 501(c)(3) Animal Sanctuary that was formerly based in Colorado. We are now located in the beautiful blue grass valleys of Kentucky. Our new location provides our current animal residents  with more land to roam freely and allows us to intake more animals in need. Lily's Ranch and Rescue focuses on the rescue, rehabilitation and re-homing of all types of farm animals. Here at Lily’s Ranch & Rescue, we provide specialized veterinary care, farrier services, custom feed, and most importantly, the tender love for all of our animals.
                     </p>
                     <p>
-                        Please visit our other pages to learn more about our mission, our animals, and how you can help us continue to provide a safe haven for animals in need.
+                        Please visit our other pages to <span onClick={renderDonate} className='donate-link'>Learn more</span> about our mission, our animals, and how you can help us continue to provide a safe haven for animals in need.
                     </p>
                 </Col>
             </Row>
