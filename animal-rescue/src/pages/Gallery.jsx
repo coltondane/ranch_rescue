fc9a6ffe3acbebab13950f8474a7dc0556349ae3
coltondane <113216904+coltondane@users.noreply.github.com>
2024-05-21 19:18:34 -0600

    working on dynamically importing all of the images to the gallery so it doesnt have to be done manually

--- animal-rescue/src/pages/Gallery.jsx
+++ animal-rescue/src/pages/Gallery.jsx
@@ -3,16 +3,20 @@ import '../style/gallery.css';
 import AnimalCount from '../components/AnimalCount';
 
 // images 
-import lilyImg from '../assets/lily.jpg';
-import pigletImg from '../assets/piglet.jpeg';
-import colt from '../assets/colt.jpeg';
-import pig2 from '../assets/pig2.jpeg';
-import turtle from '../assets/turtle.jpeg';
-import rescue1 from '../assets/rescue1.jpeg';
-import piglets from '../assets/piglets.jpeg';
-import bigPig from '../assets/hugepig.jpeg';
-import brownHorse from '../assets/horse2.jpeg';
-import donkey2 from '../assets/donkey2.jpeg';
+// import lilyImg from '../assets/lily.jpg';
+// import pigletImg from '../assets/piglet.jpeg';
+// import colt from '../assets/colt.jpeg';
+// import pig2 from '../assets/pig2.jpeg';
+// import turtle from '../assets/turtle.jpeg';
+// import rescue1 from '../assets/rescue1.jpeg';
+// import piglets from '../assets/piglets.jpeg';
+// import bigPig from '../assets/hugepig.jpeg';
+// import brownHorse from '../assets/horse2.jpeg';
+// import donkey2 from '../assets/donkey2.jpeg';
+
+function importImages(r) {
+  return r.keys().map(r);
+}
 
 const ImageGallery = () => {
 
@@ -60,18 +64,19 @@ const ImageGallery = () => {
     }
   ];
   // Array of image paths
-  const images = [
-    lilyImg,
-    pigletImg,
-    colt,
-    pig2,
-    turtle,
-    rescue1,
-    piglets,
-    bigPig,
-    brownHorse,
-    donkey2
-  ];
+  const images = importImages(require.context('../assets', false, /\.(png|jpe?g|svg)$/));
+  // const images = [
+  //   lilyImg,
+  //   pigletImg,
+  //   colt,
+  //   pig2,
+  //   turtle,
+  //   rescue1,
+  //   piglets,
+  //   bigPig,
+  //   brownHorse,
+  //   donkey2
+  // ];
 
   return (
     <>
