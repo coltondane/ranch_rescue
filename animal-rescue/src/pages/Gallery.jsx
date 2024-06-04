ba4b5273ac1ffd7fc2dc8d0e98cf90d1cd095ff5
coltondane <113216904+coltondane@users.noreply.github.com>
2024-06-04 15:26:32 -0600

    getting rid of non automated code

--- animal-rescue/src/pages/Gallery.jsx
+++ animal-rescue/src/pages/Gallery.jsx
@@ -3,18 +3,6 @@ import { useState, useEffect } from 'react';
 import '../style/gallery.css'; 
 import AnimalCount from '../components/AnimalCount';
 
-// images 
-// import lilyImg from '../assets/lily.jpg';
-// import pigletImg from '../assets/piglet.jpeg';
-// import colt from '../assets/colt.jpeg';
-// import pig2 from '../assets/pig2.jpeg';
-// import turtle from '../assets/turtle.jpeg';
-// import rescue1 from '../assets/rescue1.jpeg';
-// import piglets from '../assets/piglets.jpeg';
-// import bigPig from '../assets/hugepig.jpeg';
-// import brownHorse from '../assets/horse2.jpeg';
-// import donkey2 from '../assets/donkey2.jpeg';
-
 const images = import.meta.glob('../assets/gallery_images/*.{png,jpg,jpeg,svg}');
 
 const ImageGallery = () => {
@@ -76,19 +64,6 @@ const ImageGallery = () => {
       count: 9
     }
   ];
-  // Array of image paths
-  // const images = [
-  //   lilyImg,
-  //   pigletImg,
-  //   colt,
-  //   pig2,
-  //   turtle,
-  //   rescue1,
-  //   piglets,
-  //   bigPig,
-  //   brownHorse,
-  //   donkey2
-  // ];
 
   return (
     <>
